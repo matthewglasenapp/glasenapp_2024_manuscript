c68c6cf6b0aed8bc6e9c25371a9877bd1a621235
Matthew Glasenapp <mglasena@ucsc.edu>
2023-04-04 13:47:44 -0700

    update

--- phylonet_hmm/process_phmm_output/intersect_psg/intersect_psg.py
+++ phylonet_hmm/process_phmm_output/intersect_psg/intersect_psg.py
@@ -8,22 +8,31 @@ psg_file = "psg.csv"
 # CSV file of the genes overlapping with introgression tracts 
 introgressed_genes = "intersect.csv"
 
-with open(psg_file,"r") as file1, open(introgressed_genes,"r") as file2:
-	csv1 = csv.reader(file1)
-	csv2 = csv.reader(file2)
-	psg_list = list(csv1)
-	introgressed_genes_list = list(csv2)
+def find_psg_overlap():
+	with open(psg_file,"r") as file1, open(introgressed_genes,"r") as file2:
+		csv1 = csv.reader(file1)
+		csv2 = csv.reader(file2)
+		psg_list = list(csv1)
+		introgressed_genes_list = list(csv2)
 
-# Get list of SPU identifiers from the set of positively selected genes 
-psg_list = [n for n in psg_list[4:1012]]
+	# Get list of SPU identifiers from the set of positively selected genes 
+	psg_list = [n for n in psg_list[4:1012]]
 
-for gene in psg_list:
-	for record in introgressed_genes_list:
-		if gene[0] in record or gene[1] in record or gene[2] in record:
-			print(gene)
-			print(record[5])
-			print(record[6])
+	for gene in psg_list:
+		for record in introgressed_genes_list:
+			print(gene[0])
+			print(gene[1])
 			print(record)
-			print("Positively Selected Gene #{}".format(psg_list.index(gene)))
-			print(" ")
+			if gene[0] in record or gene[1] in record or gene[2] in record:
+				print(gene)
+				print(record[5])
+				print(record[6])
+				print(record)
+				print("Positively Selected Gene #{}".format(psg_list.index(gene)))
+				print(" ")
 
+def main():
+	find_psg_overlap()
+
+if __name__ == "__main__":
+	main()
