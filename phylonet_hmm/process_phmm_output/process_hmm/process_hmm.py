46676fc68d9744b1cebcb1a45ed2e9de64033260
Matthew Glasenapp <mglasena@ucsc.edu>
2023-05-03 10:54:33 -1000

    Update process_hmm.py

--- phylonet_hmm/process_phmm_output/process_hmm/process_hmm.py
+++ phylonet_hmm/process_phmm_output/process_hmm/process_hmm.py
@@ -13,31 +13,30 @@ import gzip
 # Number of threads to use for coverage depth analysis with mosdepth
 threads = 4
 
-# Directory for output
+# Specify the posterior probability cutoff (1-100) for declaring a site as introgressed
+posterior_probability_threshold = 90
+
+# Specify the directory for output files 
 output_dir = "/hb/scratch/mglasena/phylonet_hmm/process_hmm_90/process_hmm/"
 
-# Directory where vcf2phylip was run
-original_vcf2phylip_dir = "/hb/scratch/mglasena/phylonet_hmm/hmm_input/"
-
-# Root directory where phylonet_hmm was run
-root_dir = "/hb/scratch/mglasena/phylonet_hmm/hmm/"
-
-# Directory with json files of introgression probability arrays from cat.py
+# Specify the directory with the output from aggregate_output.py.
+# This directory contains json files of introgression probability arrays for each scaffold. 
 probability_file_dir = "/hb/scratch/mglasena/phylonet_hmm/probability_files/"
 
-# Directory containing the scaffold alignments of all sites 
+# Specify the directory containing the scaffold alignments of all sites (including monomorphic sites)
 all_sites_dir = "/hb/scratch/mglasena/phylonet_hmm/hmm_input/nexus_alignments_all_sites/"
 
-# Directory contianing the scaffold alignments used in phylonet_hmm
+# Specify the directory contianing the scaffold alignments and coordinate files used in the PhyloNet-HMM analysis
 phylonet_hmm_alignment_dir = "/hb/scratch/mglasena/phylonet_hmm/hmm_input/scaffold_nexus_alignments/"
 
-# Tsv file with scaffold names and length in base pairs
+# Specify the tsv file with scaffold names and length in base pairs
 scaffold_info_file = "/hb/home/mglasena/dissertation/scripts/phylonet_hmm/scaffolds.tsv"
 
-# File containing 100 kb gaps in the nexus alignments 
+# Specify the file identifying any 100 kb gaps present in the nexus alignments 
 gaps_file = "/hb/scratch/mglasena/phylonet_hmm/process_hmm_90/100kb_gaps.bed"
 
-# Reference alignment BAM files for assessing coverage dpeth
+# Specify the paths to the BAM files for each species used for assessing coverage depth
+# Must be in alphabetical order by species name 
 bam_file_paths_list = [
 "/hb/home/mglasena/bam_files/droebachiensis_SRR5767286_dedup_aligned_reads.bam", 
 "/hb/home/mglasena/bam_files/fragilis_SRR5767279_dedup_aligned_reads.bam", 
@@ -45,30 +44,29 @@ bam_file_paths_list = [
 "/hb/home/mglasena/bam_files/pulcherrimus_SRR5767283_dedup_aligned_reads.bam"
 ]
 
-# List of coordinate_tract_lists for each scaffold in format of [[start_coordinate, stop_coordinate, length in bp], []]
+# Initialize a list for introgression tract lists in format of [[start_coordinate, stop_coordinate, length in bp], []]
 combined_results = []
 
-# Dictionary containing scaffolds in the format {scaffold: [length in base pairs, length spanned by first and last coordinates, number of sites in all_sites alignment, number nexus sites in phmm alignment, number posterior probabilities > 90, number_tracts, number_ten_kb_tracts]}
+# Initialize a dictionary containing scaffold metrics in the format of: 
+# {scaffold: [length in base pairs, length spanned by first and last coordinates, number of sites in the all sites alignment, number nexus sites in PhyloNet-HMM alignment, number posterior probabilities > 90, number of introgression tracts, number of 10kb introgression tracts]}
 scaffold_dict = {}
 
-# Dictionary for coverage of all introgression tracts identified by phylonet-hmm
+# Initialize a dictionary for coverage depth by species of all introgression tracts identified by PhyloNet-HMM
 coverage_dict = dict()
 
-# Dictionary for coverage of filtered introgression tracts
+# Initialize a dictionary for coverage depth by species for introgression tracts passing coverage depth filters 
 filtered_coverage_dict = dict()
 
-posterior_probability_threshold = 90
-
-# Return list of phylonet_hmm output files matched with their respective coordinate files from the scaffold alignments
+# Return a list of lists of each scaffold posterior probability file matched with its respective coordinate file
 def get_file_paths_pairs_list():
 	
 	# Create file of paths of global coordinate files for each scaffold. Save this file as "coorindate_file list" 
 	os.system('find ' + phylonet_hmm_alignment_dir + ' -name "coordinates" -type f > coordinate_file_list')
 		
-	# Create file of paths of rawOutput.json files for each scaffold produced by phylonet_hmm. We only want the rawOutput.json file from the "bestrun" folder. Save this file as output_file_list in hmm directory
+	# Create file of paths to posterior probability files for each scaffold
 	os.system('find ' + probability_file_dir + ' -type f > output_file_list')
 	
-	# Get zipped list matching rawOutput.json file path to global coordinate file path for each scaffold. [[Scaffold_1_rawOutput.json path, Scaffold_1_coordinates], []]
+	# Get zipped list matching the probability file path to the global coordinate file path for each scaffold. [[Scaffold_1.json path, Scaffold_1 coordinates path], []]
 	with open("output_file_list","r") as f1:
 		output_file_path_list = f1.read().splitlines()
 	
@@ -87,12 +85,15 @@ def find_tracts_scaffold(json_file_path, coordinate_file_path):
 	coordinates = open(coordinate_file_path,"r").read().splitlines()
 	introgression_probabilities = [probability * 100 for probability in json.load(open(json_file_path,"r"))]
 
-	# Create list of lists of introgression tracts in format [[start_index,stop_index,length], [start_index,stop_index,length]]
+	# Initialize a list of lists of introgression tracts in the format of [[start index, stop index], []]
 	index_tract_list = []
+	
+	# Initialize a list of lists of introgression tracts in the format of [[start coordinate, stop coordinate, length, []]
 	coordinate_tract_list = []
+	
+	# Find indices of introgression tracts and add them to index_tract_list in the form of [Start index, Stop index]
 	start = 0
 	stop = 0
-	# Find indexes of introgression tracts and add them to index_tract_list in the form of [Start index, Stop index]
 	i = 0
 	while (i < (len(introgression_probabilities) - 1)):
 		if introgression_probabilities[i] >= posterior_probability_threshold:
@@ -109,15 +110,13 @@ def find_tracts_scaffold(json_file_path, coordinate_file_path):
 		# Format chr:coordinate. These coordinates are from the vcf file which is 1-based. Need to convert to zero-based
 		start = coordinates[tract[0]]
 		stop = coordinates[tract[1]]
-
 		chrm = start.split(":")[0]
 
-		start_coordinate = int(start.split(":")[1])
-		stop_coordinate = int(stop.split(":")[1])
-		
 		# Convert to zero-based
 		# Only need to decrement start by 1 because in bed files, the start position is 0-based. 
-		start_coordinate -= 1
+		start_coordinate = int(start.split(":")[1]) - 1
+		stop_coordinate = int(stop.split(":")[1])
+		
 		start = chrm + ":" + str(start_coordinate)
 
 		length = stop_coordinate - start_coordinate 
@@ -130,7 +129,7 @@ def find_tracts_scaffold(json_file_path, coordinate_file_path):
 
 	combined_results.append(coordinate_tract_list)
 
-# Write tract_list in format chr start stop
+# Write each introgression tract to a bed file in the format of scaffold, start, stop, name (scaffold:start_stop)
 def write_tracts_to_bed(file_name, tract_list):
 	total_number_tracts = len(tract_list)
 	print("{} introgression tracts found and written to tracts.bed".format(total_number_tracts))
@@ -146,7 +145,8 @@ def write_tracts_to_bed(file_name, tract_list):
 def intersect_tract_file_with_100kb_gaps(tract_file):
 	os.system("bedtools intersect -a " + tract_file + " -b " + gaps_file + " -wo > gap_overlap.bed")
 
-# Tracts overlapping with 100kb gaps
+# Trim or filter introgression tracts if they overlap with a 100kb missing data gap 
+# Write adjusted introgression tracts to tracts_gap_filter.bed
 def filter_tracts_for_gaps(tract_file):
 	gap_overlaps = open("gap_overlap.bed", "r").read().splitlines()
 	number_gap_overlapped_tracts = len(gap_overlaps)
@@ -246,7 +246,7 @@ def run_mosdepth(tract_file, bam_file):
 	mosdepth = "mosdepth --by " + tract_file + " --no-per-base --thresholds 1,10,20 -t {} --fast-mode {} {}".format(threads, prefix, bam_file)
 	os.system(mosdepth)
 
-# Get list of mosdepth output file paths in alphabetic order 
+# Create a list of the relevant mosdepth output file paths in alphabetic order by species name 
 def get_mosdepth_output_file_list():
 	find_files = "find {} -type f -name '*.regions*' | grep -v 'csi' > mosdepth_output_files".format(output_dir)
 	os.system(find_files)
@@ -254,7 +254,7 @@ def get_mosdepth_output_file_list():
 	os.system("rm mosdepth_output_files")
 	return sorted(mosdepth_output_file_list)
 
-# Populate coverage_dict in the format of {tract_name: coverage_species1, coverage_species2, coverage_species3, coverage_species4}
+# Populate the coverage_dict dictionary in the format of {tract_name: [coverage depth]}
 def parse_mosdepth(mosdepth_region_file):
 	with gzip.open(mosdepth_region_file,"rt") as f:
 		inputs = f.read().splitlines()
@@ -268,13 +268,14 @@ def parse_mosdepth(mosdepth_region_file):
 		else:
 			coverage_dict[tract_name].append(coverage)
 
+# Removes all mosdepth outputfiles from output directory
 def clean_up_mosdepth_output():
 	os.system("rm *.dist.txt")
 	os.system("rm *.summary.txt")
 	os.system("rm *.gz")
 	os.system("rm *.csi")
 
-# Filter coverage_dict for tracts where one sample has lower than 5x mean depth or higher than 100x mean depth
+# Filter the coverage_dict for introgression tracts where one sample has lower than 5x mean coverage depth or higher than 100x mean coverage depth. Add introgression tracts passing filter to filtered_coverage_dict. Write tracts passing filter to tracts_pf.bed. Create tract_coverage.tsv with coverage depth for each species by introgression tract.
 def filter_tracts_by_depth():
 	tracts_filtered = 0
 	tracts_pass_filter = 0 
@@ -351,6 +352,7 @@ def get_stats_on_tracts(tract_file):
 	print("Median tract length of introgression tracts longer than 10 kb: {}".format(statistics.median(ten_kb_length_dist)))
 	print("Standard deviation of introgression tracts greater than 10kb: {}".format(statistics.stdev(ten_kb_length_dist)))
 
+# Populate scaffold_dict dictionary in the format of {scaffold: [length in base pairs, length spanned by first and last coordinates, number of sites in the all sites alignment, number nexus sites in PhyloNet-HMM alignment, number posterior probabilities > 90, number of introgression tracts, number of 10kb introgression tracts]}
 def organize_tracts_by_scaffold(tract_file):
 	scaffold_list = open(scaffold_info_file,"r").read().splitlines()
 
@@ -387,6 +389,7 @@ def organize_tracts_by_scaffold(tract_file):
 		if length >= 10000:
 			scaffold_dict[scaffold][6] += 1
 
+# Write the populated scaffold_dict dictionary to a csv file called introgression_by_scaffold.csv
 def write_scaffold_dict_to_csv():
 	csv_file = open("introgression_by_scaffold.csv","w")
 	writer = csv.writer(csv_file)	
@@ -397,6 +400,7 @@ def write_scaffold_dict_to_csv():
 		writer.writerow(data)
 	csv_file.close()
 
+# Write the distribution of introgression tract lengths to a csv file called tract_length_dist.csv. Write the distribution of introgression tract lengths for introgression tracts 10kb and longer to tract_length_dist_10kb.csv.
 def write_tract_length_dist():
 	tracts = open("tracts_pf.bed").read().splitlines()
 	tract_length_dist_all = [int(tract.split("\t")[2]) - int(tract.split("\t")[1]) for tract in tracts]
@@ -412,6 +416,7 @@ def write_tract_length_dist():
 	writer.writerow(tract_length_dist_10kb)
 	csv_file_ten_kb_tracts.close()
 
+# Write introgression tracts 10kb and longer to ten_kb_tracts.bed
 def write_ten_kb_tracts():
 	tracts = open("tracts_pf.bed").read().splitlines()
 	
@@ -422,10 +427,10 @@ def write_ten_kb_tracts():
 def main():
 	os.chdir(output_dir)
 
-	# Get a list of lists of [phylonet_hmm output file, scaffold coordinate file]
+	# Get a list of lists of [posterior probability file, scaffold coordinate file] for each scaffold
 	files_by_scaffold_list = get_file_paths_pairs_list()
 
-	# Process phylonet_hmm output from each scaffold
+	# Process PhyloNet-HMM posterior probabilities from each scaffold
 	for scaffold_file_pair in files_by_scaffold_list:
 		json_file_path = scaffold_file_pair[0]
 		coordinate_file_path = scaffold_file_pair[1]
@@ -437,30 +442,48 @@ def main():
 	# Sort flat_combined_results list of all introgression tracts by tract length in base pairs in descending order. 
 	flattened_combined_results.sort(reverse=True, key=itemgetter(2))
 
+	# Write each introgression tract to a bed file in the format of scaffold, start, stop, name (scaffold:start_stop)
 	write_tracts_to_bed("tracts.bed", flattened_combined_results)
 
+	# Use bedtools intersect to fine overlap between introgression tracts and the 100 kb gaps in the scaffold alignments identified by find_gaps.py
 	intersect_tract_file_with_100kb_gaps("tracts.bed")
 
+	# Trim or filter introgression tracts if they overlap with a 100kb missing data gap 
+	# Write adjusted introgression tracts to tracts_gap_filter.bed
 	filter_tracts_for_gaps("tracts.bed")
 	
+	#Use mosdepth and tracts_gap_filter.bed to get the mean coverage depth of each introgression tract for each species
 	Parallel(n_jobs=len(bam_file_paths_list))(delayed(run_mosdepth)("tracts_gap_filter.bed", bam_file) for bam_file in bam_file_paths_list)
 
+	# Create a list of the relevant mosdepth output file paths in alphabetic order by species name 
 	mosdepth_output_file_list = get_mosdepth_output_file_list()
 
+	# Populates the coverage_dict dictionary in the format of {tract_name: [coverage depth Sdro, coverage depth Sfra, coverage depth Spal, coverage depth Hpul]}
 	for file in mosdepth_output_file_list:
 		parse_mosdepth(file)
 
+	# Removes all mosdepth outputfiles from output directory
 	clean_up_mosdepth_output()
 
+	# Filter the coverage_dict for introgression tracts where one sample has lower than 5x mean coverage depth or higher than 100x mean coverage depth. Add introgression tracts passing filter to filtered_coverage_dict. Write tracts passing filter to tracts_pf.bed. Create tract_coverage.tsv with coverage depth for each species by introgression tract.
 	filter_tracts_by_depth()
 
+	# Use mosdepth and tracts_pf.bed to get the mean coverage depth of each introgression tracts by species for tracts passing filters that will be used in downstream analyses
 	Parallel(n_jobs=len(bam_file_paths_list))(delayed(run_mosdepth)("tracts_pf.bed", bam_file) for bam_file in bam_file_paths_list)
 
+	# Print summary stats for introgression tracts passing filter 
 	get_stats_on_tracts("tracts_pf.bed")
+	
+	# Populate scaffold_dict dictionary in the format of {scaffold: [length in base pairs, length spanned by first and last coordinates, number of sites in the all sites alignment, number nexus sites in PhyloNet-HMM alignment, number posterior probabilities > 90, number of introgression tracts, number of 10kb introgression tracts]}
 	organize_tracts_by_scaffold("tracts_pf.bed")
+	
+	# Write the populated scaffold_dict dictionary to a csv file called introgression_by_scaffold.csv
 	write_scaffold_dict_to_csv()
 
+	# Write the distribution of introgression tract lengths to a csv file called tract_length_dist.csv. Write the distribution of introgression tract lengths for introgression tracts 10kb and longer to tract_length_dist_10kb.csv.
 	write_tract_length_dist()
+	
+	# Write introgression tracts 10kb and longer to ten_kb_tracts.bed
 	write_ten_kb_tracts()
 
 if __name__ == "__main__":
