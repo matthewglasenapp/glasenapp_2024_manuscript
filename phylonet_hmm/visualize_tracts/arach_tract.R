e398e538ef5000c852bc39dbae8d70d52b593e8f
Matthew Glasenapp <mglasena@ucsc.edu>
2023-05-12 16:21:36 -0700

    Update

--- phylonet_hmm/visualize_tracts/arach_tract.R
+++ phylonet_hmm/visualize_tracts/arach_tract.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(tidyverse)
 library(ggarchery)
 
-setwd("/Users/matt/desktop/hmm_R_analysis/")
+setwd("/Users/matt/Documents/Github/dissertation_chapter_2/phylonet_hmm/visualize_tracts/")
 
 introgression_probabilities = fromJSON(file = "NW_022145601.1.json")
 class(introgression_probabilities)
@@ -12,8 +12,8 @@ coordinates = read.table("NW_022145601.1_coordinates", header = FALSE)
 coordinates = as.vector(coordinates[, 1])
 
 # Create a data frame with the x and y values
-data_df <- data.frame(coordinates = coordinates[323:930], 
-                      introgression_probabilities = introgression_probabilities[323:930])
+data_df <- data.frame(coordinates = coordinates[100:1200], 
+                      introgression_probabilities = introgression_probabilities[100:1200])
 
 ggplot(data_df, aes(x = coordinates, y = introgression_probabilities)) +
   geom_line(color = "red", linetype = "solid") +
@@ -54,7 +54,51 @@ ggplot(data_df, aes(x = coordinates, y = introgression_probabilities)) +
   geom_arrowsegment(aes(x = 170401, y = 1.025, xend = 184101, yend = 1.025), arrow_positions = 0.3, arrows = arrow(length=unit(.35, 'cm'), ends = "last", type = "closed")) + 
   annotate("text", x = mean(c(170401, 184101)), y = 1.07, 
            label = "arachidonate 5-lipoxygenase", color = "black", vjust = 0) + 
+  geom_arrowsegment(aes(x = 151263, y = 1.025, xend = 162808, yend = 1.025), arrow_positions = 0.3, arrows = arrow(length=unit(.35, 'cm'), ends = "last", type = "closed")) + 
+  annotate("text", x = mean(c(151263, 162808)), y = 1.07, 
+           label = "lysosomal amino acid transporter 1", color = "black", vjust = 0) + 
+  annotate("rect", xmin = 151263, xmax = 151397, ymin = 1.00, ymax = 1.05, 
+           fill = "black", color = "black", size = 0.5) +
+  annotate("rect", xmin = 152335, xmax = 152661, ymin = 1.00, ymax = 1.05, 
+           fill = "black", color = "black", size = 0.5) +
+  annotate("rect", xmin = 153562, xmax = 153688, ymin = 1.00, ymax = 1.05, 
+           fill = "black", color = "black", size = 0.5) +
+  annotate("rect", xmin = 155242, xmax = 155350, ymin = 1.00, ymax = 1.05, 
+           fill = "black", color = "black", size = 0.5) +
+  annotate("rect", xmin = 155931, xmax = 156133, ymin = 1.00, ymax = 1.05, 
+           fill = "black", color = "black", size = 0.5) +
+  annotate("rect", xmin = 156698, xmax = 156784, ymin = 1.00, ymax = 1.05, 
+           fill = "black", color = "black", size = 0.5) +
+  annotate("rect", xmin = 157672, xmax = 157814, ymin = 1.00, ymax = 1.05, 
+           fill = "black", color = "black", size = 0.5) +
+  annotate("rect", xmin = 159949, xmax = 159976, ymin = 1.00, ymax = 1.05, 
+           fill = "black", color = "black", size = 0.5) +
+  annotate("rect", xmin = 160821, xmax = 161039, ymin = 1.00, ymax = 1.05, 
+           fill = "black", color = "black", size = 0.5) +
+  annotate("rect", xmin = 161457, xmax = 161648, ymin = 1.00, ymax = 1.05, 
+           fill = "black", color = "black", size = 0.5) +
+  annotate("rect", xmin = 162063, xmax = 162808, ymin = 1.00, ymax = 1.05, 
+           fill = "black", color = "black", size = 0.5) +
+  geom_arrowsegment(aes(x = 198738, y = 1.025, xend = 210294, yend = 1.025), arrow_positions = 0.33, arrows = arrow(length=unit(.35, 'cm'), ends = "last", type = "closed")) + 
+  annotate("text", x = mean(c(198738, 210294)), y = 1.07, 
+           label = "glycosyltransferase 8 domain-containing\nprotein 1", color = "black", vjust = 0) +
+  annotate("rect", xmin = 198738, xmax = 199186, ymin = 1.00, ymax = 1.05, 
+           fill = "black", color = "black", size = 0.5) +
+  annotate("rect", xmin = 201299, xmax = 201391, ymin = 1.00, ymax = 1.05, 
+           fill = "black", color = "black", size = 0.5) +
+  annotate("rect", xmin = 202975, xmax = 203176, ymin = 1.00, ymax = 1.05, 
+           fill = "black", color = "black", size = 0.5) +
+  annotate("rect", xmin = 203916, xmax = 204033, ymin = 1.00, ymax = 1.05, 
+           fill = "black", color = "black", size = 0.5) +
+  annotate("rect", xmin = 204438, xmax = 204626, ymin = 1.00, ymax = 1.05, 
+           fill = "black", color = "black", size = 0.5) +
+  annotate("rect", xmin = 205330, xmax = 205496, ymin = 1.00, ymax = 1.05, 
+           fill = "black", color = "black", size = 0.5) +
+  annotate("rect", xmin = 205792, xmax = 205904, ymin = 1.00, ymax = 1.05, 
+           fill = "black", color = "black", size = 0.5) +
+  annotate("rect", xmin = 206637, xmax = 210294, ymin = 1.00, ymax = 1.05, 
+           fill = "black", color = "black", size = 0.5) +
   # Add commas representing to the x axis tick marks 
   scale_x_continuous(labels = function(x) format(x, big.mark = ",", scientific = FALSE))
-
+  
 ggsave("arach.svg")
