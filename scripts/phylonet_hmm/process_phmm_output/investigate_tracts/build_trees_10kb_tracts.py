07950bed04d21832e59e4f782755b06924bae109
Matthew Glasenapp <mglasena@ucsc.edu>
2023-09-25 14:18:12 -1000

    Update

--- scripts/phylonet_hmm/process_phmm_output/investigate_tracts/build_trees_10kb_tracts.py
+++ scripts/phylonet_hmm/process_phmm_output/investigate_tracts/build_trees_10kb_tracts.py
@@ -11,6 +11,8 @@ num_cores = 24
 # 'QB3KMK016': 'pulcherrimus',
 # }
 
+# outgroup = "pulcherrimus"
+
 sample_names = {
 'QB3KMK013': 'fragilis',
 'QB3KMK010': 'franciscanus',
@@ -54,8 +56,10 @@ def subset_vcf(region):
 
 def create_fasta_alignments(region):
 	input_file = output_dir + region + "/" + region + ".vcf.gz"
-	output_directory = output_dir + region
-	run_vcf2phylip = "python3 {}vcf2phylip.py -w --input {} -p -f --output-folder {} --output-prefix {}".format(vcf2phylip_path, input_file, output_directory, region)
+	# Transition back to 0 indexed to be consistent with the tract naming via bed format
+	zero_indexed_region = region.split(":")[0] + ":" + str(int(region.split("-")[0].split(":")[1])-1) + "-" + region.split("-")[1]
+	output_directory = output_dir + zero_indexed_region
+	run_vcf2phylip = "python3 {}vcf2phylip.py -w --input {} -p -f --output-folder {} --output-prefix {}".format(vcf2phylip_path, input_file, output_directory, zero_indexed_region)
 	os.system(run_vcf2phylip)
 
 # Iqtree does not tolerate the '*'' symbol. Replace '*' with 'N'
@@ -106,21 +110,25 @@ def get_tree_file_path_list():
 
 # Replace sample id numbers with species scientific names 
 def edit_tree_files(input_file):
-	# with open(input_file, "r") as f:
-	# 	tree_list = f.read().splitlines()
+	with open(input_file, "r") as f:
+		tree_list = f.read().splitlines()
 	
 	edited_tree_file = input_file.split(".min4")[0] + ".nwk"
-	# with open(edited_tree_file,"a") as f2:
-	# 	for tree in tree_list:
-	# 		for sample_name in sample_names.keys():
-	# 			if sample_name in tree:
-	# 				new_tree = tree.replace(sample_name, sample_names[sample_name])
-	# 				tree = new_tree
-	# 		f2.write(tree + "\n")
+	with open(edited_tree_file,"a") as f2:
+		for tree in tree_list:
+			for sample_name in sample_names.keys():
+				if sample_name in tree:
+					new_tree = tree.replace(sample_name, sample_names[sample_name])
+					tree = new_tree
+			f2.write(tree + "\n")
 
 	rooted_file = edited_tree_file.split(".nwk")[0] + "_rooted.nwk"
-	root_and_prune = "{}nw_reroot {} {} | {}nw_prune - {} > {}".format(nw_utils, edited_tree_file, outgroup, nw_utils, outgroup, rooted_file)
+	root_and_prune = "nw_reroot {} {} -s | nw_prune - {} > {}".format(edited_tree_file, outgroup, outgroup, rooted_file)
 	os.system(root_and_prune)
+
+	ordered_rooted_file = rooted_file.split(".nwk")[0] + "_bl_removed.nwk"
+	order_rooted_file = "nw_topology {} | nw_order - > {}".format(rooted_file, ordered_rooted_file)
+	os.system(order_rooted_file)
 
 def update_tract_info_csv_file():
 	tract_info_lines = open(tract_info_file,"r").read().splitlines()[1:]
@@ -129,20 +137,35 @@ def update_tract_info_csv_file():
 	output_csv = open(updated_csv,"w")
 	writer = csv.writer(output_csv, delimiter = "\t")		
 
-	header = ["tract_name", "Scaffold", "Start", "Stop", "Length", "SNV Sites", "SNV/bp", "Overlapping Coding Bases", "Percent Coding", "Sdro", "Sfra", "Spal", "Hpul", "Overlapping Genes", "Sdro 1x", "Sdro 10x", "Sfra 1x", "Sfra 10x", "Spal 1x", "Spal 10x", "Hpul 1x", "Hpul 10x", "Gene Tree"]
+	header = ["tract_name", "Scaffold", "Start", "Stop", "Length", "SNV Sites", "SNV/bp", "Overlapping Coding Bases", "Percent Coding", "Sdro", "Sfra", "Spal", "Hpul", "Overlapping Genes", "Sdro 1x", "Sdro 10x", "Sfra 1x", "Sfra 10x", "Spal 1x", "Spal 10x", "Hpul 1x", "Hpul 10x", "Gene Tree", "Sdro-Spla sister?", "Bootstrap Support"]
 	writer.writerow(header)
 
 	for line in tract_info_lines:
 		length = int(line.split("\t")[4])
 		if length >= 10000:
 			tract = line.split("\t")[0].replace("-", "_", 1)
-			gene_tree_file = output_dir + tract + "/" + tract + ".nwk"
+			gene_tree_file = output_dir + tract + "/" + tract + "_rooted.nwk"
 			gene_tree = open(gene_tree_file,"r").read().split("\n")[0]
+			topology_file = output_dir + tract + "/" + tract + "_rooted_bl_removed.nwk"
+			topology = open(topology_file,"r").read().split("\n")[0]
+
+			substring = "(droebachiensis,pallidus)"
+			if substring in string:
+				dro_pal_sister = "Yes"
+				bootstrap = string.split("(droebachiensis,pallidus)")[1].split(",")[0]
+			else:
+				dro_pal_sister = "No"
+				bootstrap = "n/a"
+
 		else:
 			gene_tree = ""
+			dro_pal_sister = ""
+			bootstrap = ""
 		
 		data = line.split("\t")
 		data.append(gene_tree)
+		data.append(dro_pal_sister)
+		data.append(bootstrap)
 		writer.writerow(data)
 	
 	output_csv.close()
@@ -166,7 +189,7 @@ def main():
 
 	Parallel(n_jobs=num_cores)(delayed(edit_tree_files)(tree_file) for tree_file in tree_files)
 
-	#update_tract_info_csv_file()
+	update_tract_info_csv_file()
 
 if __name__ == "__main__":
 	main()
